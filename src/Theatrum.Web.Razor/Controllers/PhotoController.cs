ef421a6e879a65206dca4fb1695e2c0c38362918
Yaroslav Hryhorenko <yarik13371337@gmail.com>
2021-12-08 15:45:10 +0200

    add default photo

--- src/Theatrum.Web.Razor/Controllers/PhotoController.cs
+++ src/Theatrum.Web.Razor/Controllers/PhotoController.cs
@@ -2,8 +2,10 @@ using System;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+
 using Theatrum.Bl.Abstract.IServices;
 using Theatrum.Models.Models;
 using Theatrum.Utils;
@@ -25,15 +27,29 @@ namespace Theatrum.Web.Razor.Controllers
 
         [HttpGet]
         [Route("AdminPhoto/{photoId}")]
-        public async Task<FileResult> AdminPhoto(Guid photoId)
+        public async Task<FileResult> AdminPhoto(Guid? photoId)
         {
-            if (photoId == Guid.Empty)
-                return null;
-
-            PhotoModel photo = await _photoService.GetPhoto(photoId);
+            if (photoId == null || photoId == Guid.Empty)
+            {
+                return await DefaultPhoto();
+            }
+            PhotoModel photo = await _photoService.GetPhoto((Guid)photoId);
             var filePath = Path.Combine(
                 _env.ContentRootPath, Settings.UploadDirectory, photo.Name);
+            if (!System.IO.File.Exists(filePath))
+            {
+                return await DefaultPhoto();
+            }
             return PhysicalFile(filePath, photo.ContentType);
+
         }
+
+        public async Task<FileResult> DefaultPhoto()
+        {
+            var filePath = Path.Combine(
+                _env.ContentRootPath, Settings.UploadDirectory, "default.jpg");
+            return PhysicalFile(filePath, "image/jpg");
+        }
+
     }
 }
