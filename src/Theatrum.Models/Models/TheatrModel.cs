7e01dc774022d54c9879dc6aa3d0ea0b2a7db36e
Balukova <balukovvva@gmail.com>
2021-12-08 17:52:28 +0200

    ukrainian fix

--- src/Theatrum.Models/Models/TheatrModel.cs
+++ src/Theatrum.Models/Models/TheatrModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Theatrum.Models.Models
@@ -6,14 +7,18 @@ namespace Theatrum.Models.Models
     public class TheatrModel
     {
         public Guid? Id { get; set; }
+        [DisplayName("Назва")]
         [DataType(DataType.Text)]
         public string Name { get; set; }
+        [DisplayName("Опис")]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
 
+        [DisplayName("Місто")]
         [DataType(DataType.Text)]
         public string City { get; set; }
 
+        [DisplayName("Адреса")]
         [DataType(DataType.Text)]
         public string Address { get; set; }
     }
--- src/Theatrum.Web.Razor/Models/LoginViewModel.cs
+++ src/Theatrum.Web.Razor/Models/LoginViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Theatrum.Web.Razor.Models
@@ -6,22 +7,27 @@ namespace Theatrum.Web.Razor.Models
     public class RegisterViewModel
     {
         [Required]
+        [DisplayName("Ім'я")]
         public string Name { get; set; }
 
         [Required]
+        [DisplayName("Пошта")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Required]
+        [DisplayName("Пароль")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
+        [DisplayName("Підтвердити пароль")]
         [Compare(nameof(Password))]
         [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
 
         [Required]
+        [DisplayName("Пароль повторно")]
         [DataType(DataType.Date)]
         public DateTimeOffset Birthday { get; set; }
 
